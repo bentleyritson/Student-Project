dc8d8173dfe67d4a3f24857c803e51e4357336ee
richard thomas <richard.thomas@android-f36a27b9c22d20c8.interfront.local>
2018-02-08 08:39:04 +0200

    added comments

--- java/java-bugs/Change.java
+++ java/java-bugs/Change.java
@@ -1,8 +1,19 @@
 public class Change {
   public static void main(String args[]) {
+    /*
+     * Dividing each cost up into its own variable to be able to subtract in the
+     * correct order.
+     */
     double cost = 1.00;
     double cents = 0.10;
     double mycash = 2.00;
+
+    /**
+     * Because of the rounding off error in floating point numbers one should
+     * not subtract floating point numbers that is close to each other. So
+     * subtracting the 2 integers first and then the double creates the accurate
+     * result.
+     */
     System.out.println(mycash - cost - cents);
   }
 }
