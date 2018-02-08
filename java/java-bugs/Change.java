423a13ad3d0fab7bdf7e4bbe504878648fc82d74
richard thomas <richard.thomas@android-f36a27b9c22d20c8.interfront.local>
2018-02-08 07:44:08 +0200

    fixed the bug

--- java/java-bugs/Change.java
+++ java/java-bugs/Change.java
@@ -1,5 +1,8 @@
 public class Change {
-    public static void main(String args[]) {
-        System.out.println(2.00 - 1.10);
-    }
+  public static void main(String args[]) {
+    double cost = 1.00;
+    double cents = 0.10;
+    double mycash = 2.00;
+    System.out.println(mycash - cost - cents);
+  }
 }
